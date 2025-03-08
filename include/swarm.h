bd79bef2db2dfac130d2532c1f1ba15aeb0568e2
Dinamiet <iceman.sm39@gmail.com>
2025-03-08 12:15:26 +0200

    Documented

--- include/swarm.h
+++ include/swarm.h
@@ -1,13 +1,36 @@
 #ifndef _SWARM_H_
 #define _SWARM_H_
 
+/**
+ * \file
+ * Particle Swarm Optimization
+ *
+ * Particle Swarm Optimization (PSO) maximizes the objective function.
+ */
+
 #include <stddef.h>
 
 #define DIMENSIONS 1
 
+/**
+ * Objective function template which will evaluate the current particle parameters.
+ * \param parameters The current particle parameters.
+ * \return The fitness of the current particle parameters.
+ */
 typedef float (*Swarm_ObjectiveFunction)(const float* parameters);
+
+/**
+ * Random number generator template which will generate a random float between and including min and max.
+ * Uniform distribution.
+ * \param min The minimum value of the random number.
+ * \param max The maximum value of the random number.
+ * \return A random float between and including min and max.
+ */
 typedef float (*Swarm_RandomGenerator)(const float min, const float max);
 
+/**
+ * Particle information.
+ */
 typedef struct _Particle_
 {
 	float BestFitness;
@@ -16,6 +39,9 @@ typedef struct _Particle_
 	float Velocity[DIMENSIONS];
 } Particle;
 
+/**
+ * Swarm information.
+ */
 typedef struct _Swarm_
 {
 	Swarm_RandomGenerator RandomGenerator;
@@ -25,6 +51,15 @@ typedef struct _Swarm_
 	float                 GlobalBestPosition[DIMENSIONS];
 } Swarm;
 
+/**
+ * Initializes a swarm for optimization. Creates the required particles and sets their initial parameters.
+ * \param swarm The swarm to initialize.
+ * \param particles The array of particles to initialize. Should contain atleast numParticles elements.
+ * \param numParticles The number of particles in the swarm.
+ * \param randomGenerator Random number generator function to use when random numbers are needed.
+ * \param lowerBound The lower bound of the search space. All particles' positions will be within this range.
+ * \param upperBound The upper bound of the search space. All particles' positions will be within this range.
+ */
 void Swarm_Init(
 		Swarm*                      swarm,
 		Particle*                   particles,
@@ -33,8 +68,29 @@ void Swarm_Init(
 		const float                 lowerBound,
 		const float                 upperBound);
 
-void  Swarm_Optimize(Swarm* swarm, const Swarm_ObjectiveFunction evaluate, const size_t maxIterations, const float w, const float c1, const float c2);
+/**
+ * Maximizes the objective function using a particle swarm optimization algorithm.
+ * \param swarm The swarm to use for optimization.
+ * \param evaluate The objective function to maximize.
+ * \param maxIterations The maximum number of iterations to perform.
+ * \param w The inertia weight.
+ * \param c1 The cognitive coefficient.
+ * \param c2 The social coefficient.
+ */
+void Swarm_Optimize(Swarm* swarm, const Swarm_ObjectiveFunction evaluate, const size_t maxIterations, const float w, const float c1, const float c2);
+
+/**
+ * Returns the best fitness value found by the swarm.
+ * \param swarm The swarm to query.
+ * \return The best fitness value found by the swarm.
+ */
 float Swarm_GetBestFitness(const Swarm* swarm);
+
+/**
+ * Returns the position best position found by the swarm.
+ * \param swarm The swarm to query.
+ * \return The best position found by the swarm.
+ */
 float* Swarm_GetBestPosition(Swarm* swarm);
 
 #endif
--- src/particle.h
+++ src/particle.h
@@ -3,7 +3,24 @@
 
 #include "swarm.h"
 
+/**
+ * Initializes a particle with a random position and velocity.
+ * \param particle The particle to initialize.
+ * \param randomFunction The function to use for generating random positions and velocities
+ * \param lowerBound The lower bound for possible starting positions
+ * \param upperBound The upper bound for possible starting positions
+ */
 void Particle_Init(Particle* particle, const Swarm_RandomGenerator randomFunction, const float lowerBound, const float upperBound);
+
+/**
+ * Updates the position and velocity of a particle.
+ * \param particle The particle to update.
+ * \param globalBestPosition The best position found by the swarm so far.
+ * \param randomFunction The function to use for generating random cognitive and social factors.
+ * \param w The inertia weight for the velocity update.
+ * \param c1 The cognitive coefficient for the velocity update.
+ * \param c2 The social coefficient for the velocity update.
+ */
 void Particle_Update(
 		Particle*                   particle,
 		const float*                globalBestPosition,
@@ -11,6 +28,13 @@ void Particle_Update(
 		const float                 w,
 		const float                 c1,
 		const float                 c2);
+
+/**
+ * Evaluates the fitness of a particle.
+ * \param particle The particle to evaluate.
+ * \param evaluate The function to use for evaluating the fitness of the particle.
+ * \return The fitness of the particle.
+ */
 float Particle_Evaluate(Particle* particle, const Swarm_ObjectiveFunction evaluate);
 
 #endif
